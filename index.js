72ad08f26a90f6f79eecb3931fb0dafed04aca60
dnadrshin <dimankaliningrad@gmail.com>
2017-07-22 23:04:23 +0200

    change port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 5000;
+
 app.get('/', function (req, res) {
   res.send('test')
 })
 
-app.listen(80, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 80!')
 })
