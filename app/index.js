01524303b250995ebaf2a3371a1d046009f8888c
dimanklg <dimankaliningrad@gmail.com>
2017-07-23 12:48:52 +0200

    del create class

--- app/index.js
+++ app/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = React.createClass({
-  render() {
-    return (
-      <div>React test</div>
-    );
-  }
-});
+const App = () => <div>React test</div>
 
 ReactDOM.render(<App/>, document.querySelector('#app'));
